ac75b7c8887ce5fed982a3fea9155a08ddc42b1a
Michal Nowakowski <michal@nowakowski.me.uk>
2017-06-17 11:08:48 +0200

    Cwiczenie 5 - rozwiazanie'

--- google-client/src/main/java/com/infoshareacademy/api/GoogleTranslate.java
+++ google-client/src/main/java/com/infoshareacademy/api/GoogleTranslate.java
@@ -1,10 +1,10 @@
 package com.infoshareacademy.api;
 
-import java.util.HashMap;
-import java.util.Map;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
 import javax.ws.rs.core.Response;
 
 public class GoogleTranslate {
@@ -15,29 +15,18 @@ public class GoogleTranslate {
         this.API_KEY = API_KEY;
     }
 
-    private String generateQueryParams(String url, Map<String, String> params) {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(url).append("?");
-        params.forEach((key, value) -> {
-            builder.append(key).append("=").append(value).append("&");
-        });
-        return builder.toString();
-    }
-
     public String translate(String input, String source, String target) {
         final String endpoint = "https://translation.googleapis.com/language/translate/v2";
 
-        final Map<String, String> params = new HashMap<>();
-        params.put("key", API_KEY);
-        params.put("q", input);
-        params.put("source", source);
-        params.put("target", target);
-
-        final String url = generateQueryParams(endpoint, params);
+        final Form params = new Form();
+        params.param("key", API_KEY);
+        params.param("q", input);
+        params.param("source", source);
+        params.param("target", target);
 
         final Client client = ClientBuilder.newClient();
-        final WebTarget webTarget = client.target(url);
-        final Response response = webTarget.request().get();
+        final WebTarget webTarget = client.target(endpoint);
+        final Response response = webTarget.request().post(Entity.form(params));
         final String result = response.readEntity(String.class);
         response.close();
         return result;
